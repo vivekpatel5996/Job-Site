c1a066df3bd7f336196eb2fd79a0764baf8c0778
Vivek Patel <vivekjasubhai4@gmail.com>
2021-03-29 21:58:04 +0530

    Update config js

--- config.js
+++ config.js
@@ -1,19 +1,12 @@
 const env = process.env;
 
 const config = {
-  // db: { 
-  //   /* don't expose password or any sensitive info, done only for demo */
-  //   host: env.DB_HOST || 'freedb.tech',
-  //   user: env.DB_USER || 'freedbtech_tarun',
-  //   password: env.DB_PASSWORD || '$tarun$',
-  //   database: env.DB_NAME || 'freedbtech_tarunjobsite',
-  // },
-  db: {
+   db: {
     /* don't expose password or any sensitive info, done only for demo */
-    host: env.DB_HOST || 'aa1v756wiy0upx2.ck4kzk4esbfk.us-east-2.rds.amazonaws.com',
-    user: env.DB_USER || 'root',
-    password: env.DB_PASSWORD || 'admin123',
-    database: env.DB_NAME || 'job_site'
+    host: env.DB_HOST,
+    user: env.DB_USER,
+    password: env.DB_PASSWORD,
+    database: env.DB_NAME
   }
 };
 
