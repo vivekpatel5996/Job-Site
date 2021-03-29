fb145785cf8fe3c9a61493c4df970ffe96e99558
Vivek Patel <vivekjasubhai5@gmail.com>
2021-03-29 21:37:24 +0530

    Updated DB config for AWS

--- config.js
+++ config.js
@@ -1,13 +1,20 @@
 const env = process.env;
 
-const   config = {
-  db: { /* don't expose password or any sensitive info, done only for demo */
-    host: env.DB_HOST || 'freedb.tech',
-    user: env.DB_USER || 'freedbtech_tarun',
-    password: env.DB_PASSWORD || '$tarun$',
-    database: env.DB_NAME || 'freedbtech_tarunjobsite',
-  },
-  listPerPage: env.LIST_PER_PAGE || 10,
+const config = {
+  // db: { 
+  //   /* don't expose password or any sensitive info, done only for demo */
+  //   host: env.DB_HOST || 'freedb.tech',
+  //   user: env.DB_USER || 'freedbtech_tarun',
+  //   password: env.DB_PASSWORD || '$tarun$',
+  //   database: env.DB_NAME || 'freedbtech_tarunjobsite',
+  // },
+  db: {
+    /* don't expose password or any sensitive info, done only for demo */
+    host: env.DB_HOST || 'aa1v756wiy0upx2.ck4kzk4esbfk.us-east-2.rds.amazonaws.com',
+    user: env.DB_USER || 'root',
+    password: env.DB_PASSWORD || 'admin123',
+    database: env.DB_NAME || 'job_site'
+  }
 };
 
 
